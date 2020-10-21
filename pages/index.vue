9e2445eaf2c39227cd149c289e8d696463c7ad9f
tkgstrator <nasawake.am@gmail.com>
2020-10-21 19:39:09 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header class="navigation-bar">
+      <h1>Salmon Run Records</h1>
+    </header>
+    <div class="page coop-content">
+      <div class="coop-content-header">
+        <h2><span>Salmon Run</span></h2>
+        <h3><span>LanPlay Records</span></h3>
+      </div>
+      <div class="coop-content-list-wrapper">
+        <div id="coop-content-list"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "~assets/sass/splatnet2.scss";
+</style>
